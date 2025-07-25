d20397a0d8822fd15ad4e3434dd40dbc090fe665
Duc Tung <hbtthangtien@gmail.com>
2025-07-25 16:53:37 +0700

    feat(base project):init base project following N-Layer architecture

---
+++ Application/Extensions/DependenceInjection.cs
@@ -0,0 +1,26 @@
+﻿using CoreLayer.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Extensions
+{
+    public static class DependenceInjection
+    {
+        public static void ConfigDatabaseInject(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<CloneEbayDbContext>(options =>
+            {
+                options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
+            });
+        }
+
+
+
+    }
+}
---
+++ Ebay/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Ebay.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
